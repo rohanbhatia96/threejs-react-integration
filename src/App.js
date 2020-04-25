7552503444f882afad212b16828e09ff91009a79
Rohan Bhatia <mr.bhatia07@gmail.com>
2020-04-26 05:24:00 +0530

    Added ThreeCube to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ThreeCube from "./ThreeCube";
 
 const App = () => {
   return (
     <div>
       <h1>Three.js-React Integration</h1>
+      <ThreeCube />
     </div>
   );
 };
